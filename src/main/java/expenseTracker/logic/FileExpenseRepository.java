ab08f806f5e8415401695eedce3295a71a0e03ee
KacperGolubski <mkacpergolubski@gmail.com>
2026-04-16 00:15:55 +0200

    Added FileExpenseRepository file

---
+++ src/main/java/expenseTracker/logic/FileExpenseRepository.java
@@ -0,0 +1,35 @@
+package expenseTracker.logic;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import expenseTracker.domain.Expense;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+
+public class FileExpenseRepository {
+    private ObjectMapper objectMapper;
+    private File file;
+    public List<Expense> loadExpenses() {
+        try{
+            List<Expense> expenses= objectMapper.readValue(file, new TypeReference<List<Expense>>(){});
+            return expenses;
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+    public void saveExpense(List <Expense> expenses) {
+        try{
+            objectMapper.writeValue(file, expenses);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public FileExpenseRepository(String filePath){
+        this.file = new File(filePath);
+        this.objectMapper = new ObjectMapper();
+    }
+}
