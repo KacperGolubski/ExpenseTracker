9b8b2a7c8ebecfdf50fd475c4ee97916feca85f9
KacperGolubski <mkacpergolubski@gmail.com>
2026-04-16 23:41:26 +0200

    Updated main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import expenseTracker.logic.ExpenseService;
+import expenseTracker.logic.FileExpenseRepository;
+
 public class Main {
+    public static void main(String[] args) {
+        FileExpenseRepository fileRepository = new FileExpenseRepository("expenses.json");
+        ExpenseService expenseService = new ExpenseService(fileRepository);
+    }
 }
