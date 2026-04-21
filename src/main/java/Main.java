5da2def84cc681a149ee362fe2a2ebd89f8daf2f
KacperGolubski <mkacpergolubski@gmail.com>
2026-04-21 22:26:46 +0200

    Added addExpense method to menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
+import expenseTracker.domain.Expense;
+import expenseTracker.domain.ExpenseType;
 import expenseTracker.logic.ExpenseService;
 import expenseTracker.logic.FileExpenseRepository;
+import java.time.LocalDate;
 import java.util.Scanner;
 
+
 public class Main {
+    static FileExpenseRepository fileRepository = new FileExpenseRepository("expenses.json");
+    static ExpenseService expenseService = new ExpenseService(fileRepository);
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        FileExpenseRepository fileRepository = new FileExpenseRepository("expenses.json");
-        ExpenseService expenseService = new ExpenseService(fileRepository);
-        Scanner scanner = new Scanner(System.in);
         boolean isRunnig = true;
         while (isRunnig) {
             System.out.println("\n--- MAIN MENU ---\n");
@@ -22,7 +26,8 @@ public class Main {
                 case "1":
                     System.out.println("Provide expense ID");
                 case "2":
-                    System.out.println("Add new expense");
+                    System.out.println("Provide expense details");
+                    addExpenseMain();
                     break;
                 case "3":
                     System.out.println("Update expense");
@@ -41,5 +46,32 @@ public class Main {
                     System.out.println("Invalid choice");
             }
         } scanner.close();
+    }
+    public static void addExpenseMain(){
+        Expense expense = null;
+        System.out.println("Enter expense name");
+        String name = scanner.nextLine();
+        System.out.println("Enter expense shop name");
+        String shopName = scanner.nextLine();
+        System.out.println("Enter expense description");
+        String description = scanner.nextLine();
+        System.out.println("Enter expense price");
+        double price = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter expense date in format YYYY-MM-DD");
+        LocalDate date = LocalDate.parse(scanner.nextLine());
+        System.out.println("Enter expense type");
+        ExpenseType type = ExpenseType.valueOf(scanner.nextLine().toUpperCase());
+        try{
+            expense = new Expense(name, shopName, description, price, date, type);
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+        if(expenseService.addExpense(expense)){
+            System.out.println("Expense added successfully");
+        } else  {
+            System.out.println("Expense could not be added");
+        }
     }
 }
